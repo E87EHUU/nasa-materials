5a738d91dfd5331679db6f3b4885bbcacb92c782
E87EHUU <e_zvyagin@mail.ru>
2023-05-14 22:13:36 +0300

    Урок 1 | Наследовался от AppCompatImageView (дополнение)

--- app/src/main/java/com/example/nasa_materials/utils/Utils.kt
+++ app/src/main/java/com/example/nasa_materials/utils/Utils.kt
@@ -1,3 +1,15 @@
 package com.example.nasa_materials.utils
 
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
 const val NASA_ENDPOINT = "planetary/apod"
+
+class MyImageView @JvmOverloads constructor (context: Context, attributeSet: AttributeSet, defStyle:Int=0):
+    AppCompatImageView(context,attributeSet,) {
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+    }
+}
--- app/src/main/java/com/example/nasa_materials/viewmodel/PictureOfTheDayViewModel.kt
+++ app/src/main/java/com/example/nasa_materials/viewmodel/PictureOfTheDayViewModel.kt
@@ -11,7 +11,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class PictureOfTheDayViewModel(private val liveData: MutableLiveData<AppState>,
+class PictureOfTheDayViewModel(private val liveData: MutableLiveData<AppState> = MutableLiveData<AppState>(),
                                private val repositoryImpl:RepositoryImpl= RepositoryImpl()):
     ViewModel() {
 
